57d38ec15067178fb314a4efacc5e0420441cda1
King-alex-d-great <ogubuikealex@gmail.com>
2022-01-18 12:42:35 +0100

    Added Logic for employee application

---
+++ contracts/RecommendationPortal.sol
@@ -0,0 +1,37 @@
+//SPDX-License-Identifier: UNLICENSED 
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract RecommendationPortal {
+    constructor(){
+        console.log("yo yo oy, I don det create buildspace rpoject!");
+    }
+   
+   //should have an event!!
+      uint totalAboutMes;
+
+   mapping (address => string) employeeToAboutme;
+    //Am an employer, this is a contract where you send info about your self nd why i should employ u
+
+    //I want a person to be able to send me an about me
+    function sendAboutMe (string memory aboutme) public {
+        totalAboutMes += 1;
+        _saveAboutMe(msg.sender, aboutme);
+        console.log("%s just sent you an about me %s", msg.sender, aboutme);
+    }
+    // i want to be able to save peoples aboutme 
+
+    function _saveAboutMe (address sender, string memory aboutme) private {
+        //require that the person has not sent stuff ever before!!
+        employeeToAboutme[sender] = aboutme;
+        //emit an event! 
+    }
+
+    //I want to be able to get the number of about mes i have
+    function getTotalAboutMes () public view returns (uint) {
+        console.log("We have %d potential employees", totalAboutMes);
+        return totalAboutMes;
+    }
+}
