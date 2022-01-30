1b8d8e558a3852f089763a8240c0dc7a5a02059a
Alexandra ogubuike <ogubuikealex@gmail.com>
2022-01-30 09:39:26 +0100

    Deployed to Rinkeby

--- februryproject/scripts/deploy.js
+++ februryproject/scripts/deploy.js
@@ -1,28 +1,24 @@
 const main = async () => {
-    //get the address of the deployer
-    const [deployer] = await hre.ethers.getSigners();
-    //get balance
-    let balance = await deployer.getBalance();
-    
-    console.log(`The deployer of this app : ${deployer.address}`);
-    console.log(`deployer balance : ${balance}`);
-    //get the contract
 
-    var contractFactory = await hre.ethers.getContractFactory("RecommendationPortal");
-    var contract = await contractFactory.deploy({value: hre.ethers.utils.parseEther("0.1")});
-    await contract.deployed();
-    
-    console.log("WavePortal address: ", contract.address);
+    const [deployer] = await hre.ethers.getSigners();
+
+    const wagmiContractFactory = await hre.ethers.getContractFactory("WagmiTkn");
+
+    const wagmiContract = await wagmiContractFactory.deploy();
+    await wagmiContract.deployed();
+
+    console.log(`Deployed by: ${deployer.address}`);
+    console.log(`Deployed to: ${wagmiContract.address}`);
 }
 
-const run = async () => {
-    try {
-        await main();
-        process.exit(0);
-    } catch (error) {
-        console.log(error);
-        process.exit(0);
-    }
+const run = async () => {   
+        await main()
+        .then(() => process.exit(0))
+        .catch(err =>{
+            console.log(`error occured: ${err}`)
+        process.exit(1);
+        });        
+      
 }
 
 run();
