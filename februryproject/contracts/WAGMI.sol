2c15be42aeecc0a9a2b8b81506d0e7c49d93e366
Alexandra ogubuike <ogubuikealex@gmail.com>
2022-02-02 05:24:44 +0100

    Update

--- februryproject/contracts/WAGMI.sol
+++ februryproject/contracts/WAGMI.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Wagmi is ERC20 {
-    constructor(string memory name, string memory symbol) ERC20("Wagmi", "WAGMI") {
+    constructor() ERC20("Wagmi", "WAGMI") {
         _mint(msg.sender, 10000000 *(10 ** 18));
     }
 }
