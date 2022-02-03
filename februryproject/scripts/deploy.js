9df6aa1c8d4996e05c6d40085f151919daca8f78
Alexandra ogubuike <ogubuikealex@gmail.com>
2022-02-03 02:56:07 +0100

    Update

--- februryproject/scripts/deploy.js
+++ februryproject/scripts/deploy.js
@@ -3,12 +3,18 @@ const main = async () => {
     const [deployer] = await hre.ethers.getSigners();
 
     const wagmiContractFactory = await hre.ethers.getContractFactory("WagmiTkn");
+    const wagmiTwoContractFactory = await hre.ethers.getContractFactory("WAGMI");
 
     const wagmiContract = await wagmiContractFactory.deploy();
     const wagmi = await wagmiContract.deployed();
 
+    const wagmi2Contract = await wagmiTwoContractFactory.deploy();
+    const wagmi2 = await wagmi2Contract.deployed();
+
     console.log(`Deployed by: ${deployer.address}`);
     console.log(`Deployed to: ${wagmi.address}`);
+
+    console.log(`Deployed to: ${wagmi2.address}`);
 }
 
 const run = async () => {   
