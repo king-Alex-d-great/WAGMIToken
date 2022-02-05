e7f6d67860c865de37a8a41c7758fa1b8694ce17
Alexandra ogubuike <ogubuikealex@gmail.com>
2022-02-05 23:45:03 +0100

    Added Web3 library

--- februryproject/scripts/NFTDeploy.js
+++ februryproject/scripts/NFTDeploy.js
@@ -1,7 +1,7 @@
 const main = async () => {
     const nftFactory = await hre.ethers.getContractFactory("NFTite");
 
-    const nFtContract = await nftFactory.deploy();
+    const nFtContract = await nftFactory.deploy(); //Calling deploy() on a ContractFactory will start the deployment, and return a Promise that resolves to a Contract.
     let contrat = await  nFtContract.deployed();
     console.log(contrat.address)
 }
