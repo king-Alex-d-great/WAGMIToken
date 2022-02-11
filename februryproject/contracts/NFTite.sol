3300295d958181e9f2ee175dc72c43e9b723fe27
Alexandra ogubuike <ogubuikealex@gmail.com>
2022-02-11 16:28:44 +0100

    Update

--- februryproject/contracts/NFTite.sol
+++ februryproject/contracts/NFTite.sol
@@ -30,9 +30,10 @@ contract NFTite is ERC721URIStorage, ERC721Enumerable, Ownable, ReentrancyGuard 
     mapping(address => mapping(uint => uint)) private _tokenToTimeGivenAway;
 
     //tokenId to amount available/minted
-    mapping(uint => uint) private tokenAvailable;    
+    mapping(uint => uint) private tokenAvailable;   
+    //store six months as a private field Six months = now (182.50000182500003 * 1days ) 
 
-    constructor () public ERC721("NFTite", "nftite") {}
+    constructor () public payable ERC721("NFTite", "nftite") {}
     event MintSucces ( address sender);
 
     function mintNft (address recipient, string memory tokenURI) public onlyOwner returns (uint){        
@@ -59,6 +60,42 @@ contract NFTite is ERC721URIStorage, ERC721Enumerable, Ownable, ReentrancyGuard 
     }
 
     function SellNft (uint tokenId) external nonReentrant {
+        //check that person doing this is the owner of the NFT
+        tokenAvailable[tokenId] +=  1;
+        let timeOfHold = _calculateHoldTime();
+        let cashoutAmount = _calculateCashoutAmount(timeOfhold,tokenId, addressi);
+        //emit event if the person time acquired is not up to six months
+        _safeTransfer(msg.sender, _contractWalletAddress, tokenId);
+        payable(msg.sender).transfer(cashoutAmount);
+    }
+
+    function GetAllHolders () external view {
+        //Use a struct
+    }
+
+    function _calculateHoldTime(uint tokenId, address holder) private pure returns (uint, bool){
+    //get time acquired
+    //get time right now! 
+    //timeheld = time right now - time acquired = result 
+    //convert timeheld to months
+    //use either bool or uint as an indicator to check whether this person has been holding for up to six months or less, Store this result in a variable
+    //return timeOfHold and then indicator if it is less than 6 months
+    }
+
+    function _calculateCashoutAmount (uint initialInvestment) private pure returns (uint) {
+        //get initial amount of token
+        //get the timeOfHold
+        uint amountPerday = initialInvestment / 365 days;
+
+        //if time of Hold is greater than 6 months       	
+	   uint amountToGet = amountPerday * daysHeld;
+
+       //if timeofhold is less than 6 months
+	   uint amountToGet = 30/100 * initialInvestment;
+       //182.50000182500003 (6 months) half of 365 days.
+    }
+
+    function withdraw() external onlyOwner reentrant {
 
     }
 
